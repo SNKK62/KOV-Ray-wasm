f22b819ef9cff95a8d1c131cec4b00e9581f3ea8
SNKK62 <koki.seno62@gmail.com>
2024-12-09 00:33:01 +0900

    add kov-ray syntax highlight

--- web/src/editor.tsx
+++ web/src/editor.tsx
@@ -1,4 +1,5 @@
 import CodeMirror, { ViewUpdate } from '@uiw/react-codemirror';
+import { KOVRay } from 'codemirror-lang-kov-ray';
 
 type EditorProps = {
   value: string;
@@ -12,12 +13,12 @@ export const Editor = ({ value, onChange }: EditorProps) => {
         height="calc(100vh - 8.25rem)"
         width="50vw"
         value={value}
-        // extensions={[javascript({ jsx: true })]}
+        extensions={[KOVRay()]}
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         onChange={(val: string, _viewUpdate: ViewUpdate) => {
           onChange(val);
         }}
-        // theme="dark"
+        theme="dark"
       />
     </div>
   );
